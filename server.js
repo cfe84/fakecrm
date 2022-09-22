18917c54b676f1fdeb047b139ecbaadfce5328dd
Charles Feval <fevalc>
2022-09-22 16:38:41 -0700

    PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Store = require("./store");
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = Number.parseInt(process.env.PORT || "3000");
 
 const store = Store("store.json");
 
